a2342562389435b830f09a475bc2750223ef8072
TAE IN KIM <kuio19364@gmail.com>
2020-05-07 14:54:25 +0900

    Updates

---
+++ precache-manifest.b6b5f5fa347b26d0cf8040a015c7b14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b3025b45c47688c4f87c931ec5877d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "315d40d1c26a49926812",
+    "url": "/movie_app/static/css/main.9b891b1f.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315d40d1c26a49926812",
+    "url": "/movie_app/static/js/main.4e3e0e96.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
